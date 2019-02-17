142dab505400b47cfd2b3830db9d0732d7db5341
Mark <markhovsepyan98@gmail.com>
2019-02-18 01:25:53 +0400

    Changed plot

--- Assignment 1/assignment_1_3.R
+++ Assignment 1/assignment_1_3.R
@@ -1,18 +1,18 @@
-## libraries used in this script, please install  them before running the code
+## libraries used in this script, please install them before running the code
 library(ggplot2)
 library(RColorBrewer)
 
-## data fetching and preparation
+## Data fetching and preparation
 pokemons <- read.csv('Pokemon.csv', stringsAsFactors = F)
 pokemons$Legendary <- as.integer(as.logical(pokemons$Legendary))
 
-## Custom pallete
-new_pallete <- colorRampPalette(brewer.pal(5, 'OrRd'))
+## Custom palette
+new_palette <- colorRampPalette(brewer.pal(5, 'OrRd'))
 
 ## Modified plot
 ggplot(pokemons, aes(Type.1)) +
   geom_bar(aes(fill=..count..), alpha=0.8) +
-  scale_fill_gradientn(colours = new_pallete(100)) +
+  scale_fill_gradientn(colours = new_palette(100)) +
   theme(axis.text.x = element_text(angle = 20, hjust = 0, colour = "#B30000"),
         axis.text.y = element_text(angle = 20, hjust = 0, colour = "#050711"),
         panel.grid.major = element_blank(), 
@@ -25,7 +25,16 @@ ggplot(pokemons, aes(Type.1)) +
   ggtitle("Distribution of Type 1 by Generation") +
   coord_polar() +
   facet_wrap(. ~ Generation) +
-  theme(strip.text.x = element_text(size = 8, colour = "#B30000"),
+  theme(strip.text.x = element_text(size = 12, face = "bold", colour = "#B30000"),
         strip.text.y = element_text(size = 12, face = "bold", colour = "#B30000"),
-        strip.background = element_rect(colour = "#050711", fill = "#050711"))
+        strip.background = element_rect(colour = "#050711", fill = "#050711"),
+        legend.background = element_rect(fill = "#050711", 
+                                         size=0.5, linetype="solid"),
+        legend.title = element_text(colour = "#B30000", size = 10, 
+                                    face = "bold"),
+        legend.text = element_text(colour = "#B30000", size = 10, 
+                                   face = "bold"))
 
+
+
+  
